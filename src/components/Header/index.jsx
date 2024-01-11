37b10628583df1dd620ca843a4d010bba6471ab5
beyzaulk <beyzaulkumen.98@gmail.com>
2024-01-11 16:38:30 +0300

    Add functionality to create a new task

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,14 @@
+import Logo from "../../assets/Logo.svg";
+import styles from "./header.module.css";
+
 export function Header() {
   return (
-    <header>
-      <p>header</p>
+    <header className={styles.header}>
+      <img src={Logo} />
+      <form className={styles.newTaskForm}>
+        <input placeholder="add a new task" type="text" />
+        <button>Create</button>
+      </form>
     </header>
   );
 }
