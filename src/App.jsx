0a612598e98d21647175c5ebc0f3a1005e79c4df
beyzaulk <beyzaulkumen.98@gmail.com>
2024-01-11 15:29:11 +0300

    Add font styles to index.html

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-
+import { Header } from "./components/Header";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <p></p>
+      <Header />
     </>
   );
 }
