14d30bf6b6789b78dff6139188f9457c17e121b9
beyzaulk <beyzaulkumen.98@gmail.com>
2024-01-11 16:53:21 +0300

    Add project logo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,18 @@
+import React from "react";
+
 import Logo from "../../assets/Logo.svg";
 import styles from "./header.module.css";
 
 export function Header() {
   return (
-    <header className={styles.header}>
-      <img src={Logo} />
-      <form className={styles.newTaskForm}>
-        <input placeholder="add a new task" type="text" />
-        <button>Create</button>
-      </form>
-    </header>
+    <div>
+      <header className={styles.header}>
+        <img src={Logo} />
+        <form className={styles.newTaskForm}>
+          <input placeholder="add a new task" type="text" />
+          <button>Create</button>
+        </form>
+      </header>
+    </div>
   );
 }
